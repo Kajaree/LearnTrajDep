4bf63d82803a86dcea9b76c1bef38634dc84e069
Kajaree <kajarees@gmail.com>
2020-04-02 10:52:05 +0200

    add trained model

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#block(name=block-1, threads=5, memory=2000, subtasks=1, gpus=1, hours=24)
-python3 main.py --data_dir "../data/h3.6m/dataset/" --epoch 10 --input_n 50 --output_n 50 --dct_n 35 --exp "logs/"
+#block(name=block-1, threads=5, memory=15000, subtasks=1, gpus=1, hours=24)
+python3 main.py --data_dir  "../data/h3.6m/dataset/" --epoch 10 --input_n 100 --output_n 100 --dct_n 200 --exp "logs/"
