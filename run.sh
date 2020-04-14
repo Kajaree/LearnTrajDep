4241158ed066cafab5a24e8a008c7a95c0eacbc4
Kajaree <kajarees@gmail.com>
2020-04-14 11:37:03 +0200

    New models

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #block(name=block-1, threads=5, memory=15000, subtasks=1, gpus=1, hours=24)
-python3 main.py --data_dir  "../data/h3.6m/dataset/" --epoch 50 --input_n 10 --output_n 50 --dct_n 60 --exp "logs/"
+python3 main.py --data_dir  "../data/h3.6m/dataset/" --epoch 50 --input_n 25 --output_n 50 --dct_n 75 --exp "logs/"
